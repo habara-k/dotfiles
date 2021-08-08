ee7d0f202db56137c9feeec8371e889cd50cdbc8
Keigo Habara <k.habara.aa@gmail.com>
2021-08-09 02:18:19 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,3 @@ ln -snfv $DOT_DIRECTORY/tmux.conf $HOME/.tmux.conf
 
 mkdir -p $HOME/.config/nvim
 ln -snfv $DOT_DIRECTORY/config/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -snfv $DOT_DIRECTORY/config/nvim/dein.toml $HOME/.config/nvim/dein.toml
